083311b3c37e3d8f5ec9f830ff45e7cbd579c6e3
Tanishq0007 <tanishq16verma@gmail.com>
2020-09-09 00:29:46 +0530

    Added Button To The Modal

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Aux from '../../../HigerOrderComponent/Auxilliary';
+import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
     const ingredientSummary = Object.keys(props.ingredients)
@@ -18,6 +19,8 @@ const orderSummary = (props) => {
             </ul>
             <p>Continue to Checkout ?</p>
             <p>The Total Amount is {props.price.toFixed(2)}</p>
+            <Button btnType="Danger" clicked={props.purchaseCancelled} >CANCEL</Button>
+            <Button btnType="Success" clicked={props.purchaseContinued} >CONTINUE</Button>
         </Aux>
     )
 };
--- src/container/BurgerBulder/BurgerBuilder.js
+++ src/container/BurgerBulder/BurgerBuilder.js
@@ -78,6 +78,10 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: false});
     }
 
+    purchaseContinueHandler = () => {
+        alert('You Continue!')
+    }
+
     render () {
         const disabledinfo = {
             ...this.state.ingredients
@@ -89,6 +93,8 @@ class BurgerBuilder extends Component {
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed = {this.purchaseCancelHandler} >
                     <OrderSummary 
+                        purchaseCancelled = {this.purchaseCancelHandler}
+                        purchaseContinued = {this.purchaseContinueHandler}
                         ingredients = {this.state.ingredients}
                         price={this.state.totalPrice} />
                 </Modal>
