6733595c0028c13b08921e20c1871d8cd95e11fe
Tanishq0007 <tanishq16verma@gmail.com>
2020-09-10 00:54:53 +0530

    SettingUp The SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Aux from '../../HigerOrderComponent/Auxilliary';
 import './Layout.css';
-import Toolbar from '../Navigation/Toolbar/Toolbar'
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <main className = 'content'>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    }
 
-export default layout;
+    sideDrawerCloseHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+    
+    render() {
+        return (<Aux>
+            <Toolbar />
+            <SideDrawer  
+                closed = { this.sideDrawerCloseHandler }
+                open = { this.showSideDrawer }/>
+            <main className = 'content'>
+                {this.props.children}
+            </main>
+        </Aux>
+        )}
+}
+
+export default Layout;
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import burgerLogo from '../../assets/images/burger-logo.png';
 import './Logo.css';
 
 const logo = (props) => (
-    <div className='Logo'>
+    <div className='Logo' style={{height: props.height, margin: props.marginBottom}}>
         <img src={burgerLogo} alt="MyBurger" />
     </div>
 );
